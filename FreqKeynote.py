4f2f1631927a2631c97f046b5bdfd54ae94ddd9d
Carolin Straub <carostra@users.noreply.github.com>
2016-10-09 15:09:26 +0200

    Update FreqKeynote.py

--- FreqKeynote.py
+++ FreqKeynote.py
@@ -1,15 +1,10 @@
 #Frequency of the keynote of claves
 import numpy as np 
 
-x,t,rate=read(’R1.wav’)
-
+x,t,rate=read(’R.wav’)
 f=np.fft.fft(x,rate) 
-
-ft=np.fft.fftshift(np.fft.fft(x,rate)) #centering fft around zero
-
-faxis=np. arange(−ft . shape [0]/2 , ft . shape [0]/2)∗1./ rate #Vector with points in time (axis of abscissae)
-
+ft=np.fft.fftshift(np.fft.fft(x,rate)) 
+faxis=np.arange(−ft.shape[0]/2,ft.shape[0]/2)∗1./rate 
 t=faxis[np.where(np.abs(ft)==np.amax(np.abs(ft)))[0]] 
-#Time values of the maximum of the absolute value (negative values possible) of the Fourier transform
-
 n=int(round(np.abs(22.05/t)))
+
