74f53c841d0014a67163f590f8d1f328a1b5caf1
Carolin Straub <carostra@users.noreply.github.com>
2016-10-11 11:34:55 +0200

    Update Read.py

--- Read.py
+++ Read.py
@@ -5,5 +5,5 @@ import numpy as np
 def read(filename):
     rate,x=wavfile.read(filename) 
     x = x.astype(np.float) 
-    t = np.arange(x.shape[0])*1./rate*100
+    t = np.arange(x.shape[0])*1./rate*1000
     return x,t,rate
