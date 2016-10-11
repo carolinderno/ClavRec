6d2f0f16c15f1bc6adfabb496dd52c088b72849f
Carolin Straub <carostra@users.noreply.github.com>
2016-10-11 16:55:43 +0200

    Update RedDim.py

--- RedDim.py
+++ RedDim.py
@@ -1,5 +1,5 @@
 #Dimension reduction 
-#Returning a vector with the points in time of the local maxima of all amplitude envelopes
+#Returning a vector with the points in time of the local maxima of all amplitude envelopes (= onsets)
 #bounds s1 (y-axis) and s2 (x-axis) were adapted to the specific claves' recordings
 #in order to get all maxima of all rhythms
 
