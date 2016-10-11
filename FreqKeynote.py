ccf6460985d6655ea59e34984ba0f49fceaee672
Carolin Straub <carostra@users.noreply.github.com>
2016-10-11 11:30:59 +0200

    Update FreqKeynote.py

--- FreqKeynote.py
+++ FreqKeynote.py
@@ -1,7 +1,7 @@
 #Frequency of the keynote of claves
 import numpy as np 
 
-x,t,rate=read(’R.wav’)
+x,t,rate=read('R.wav')
 f=np.fft.fft(x,rate) 
 ft=np.fft.fftshift(np.fft.fft(x,rate)) 
 faxis=np.arange(−ft.shape[0]/2,ft.shape[0]/2)∗1./rate 
